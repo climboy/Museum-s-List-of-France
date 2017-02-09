09a4d4cdc800d9924c378691ece12173cc1750e9
cedric70 <cedriccodeur@gmail.com>
2017-02-09 11:46:56 +0100

    Update recherche.php

--- recherche.php
+++ recherche.php
@@ -2,90 +2,101 @@
 try{
   $bdd = new PDO('mysql:host=localhost;dbname=musee;charset=utf8', 'root', '');
 }
-
 catch (Exception $e){
   die('Erreur : ' . $e->getMessage());
 }
+if(isset($_GET['search']))
+{
+  $recherche=$_GET['search'];  
+  $reponses = $bdd->query("SELECT * FROM musee WHERE VILLE LIKE '%$recherche%' OR ADR LIKE '%$recherche%' OR NOMDUMUSEE LIKE '%$recherche%' OR NOMDEP LIKE '%$recherche%' OR NOMREG LIKE '%$recherche%' OR CP LIKE '%$recherche%'  ORDER BY VILLE ASC");
+}
+else{
+  $reponses = $bdd->query("SELECT * FROM musee ORDER BY VILLE ASC");
+}
 
-$reponses = $bdd->query("SELECT * FROM musee ORDER BY VILLE ASC");
-
-
-  ?>
+?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="Bootstrap/css/Bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Musées</title>
-  </head>
-  <body>
-    <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
- 	     <span class="sr-only">Toggle navigation</span>
- 	     <span class="icon-bar"></span>
- 	     <span class="icon-bar"></span>
- 	     <span class="icon-bar"></span>
- 	   </button>
- 	 </div>
- 	 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
- 	              <ul class="nav navbar-nav">
- 	                  <li class="col-sm-4"><a href="accueil.php">Accueil</a></li>
- 	                  <li class="col-sm-4"><a href="recherche.php">Musées</a></li>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+  <link rel="stylesheet" href="Bootstrap/css/Bootstrap.min.css">
+  <link rel="stylesheet" href="css/recherche.css">
 
- 	              </ul>
+  <title>Musées</title>
+</head>
+<body>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right" style="width:100%;">
+          <li class="col-sm-4"><a class="valign" href="accueil.php">Accueil</a></li>
+          <li class="col-sm-4"><a class="valign" href="recherche.php">Musées</a></li>
+          <li class="col-sm-4">
+            <form class="form-group" method="GET" action="recherche.php">
+              <div class="input-group input-group-md icon-addon vpadding">
+                <input type="text" placeholder="Texte" name="search" id="schbox" class="form-control">
+                <i class="icon icon-search"></i>
+                <span class="input-group-btn">
+                  <button type="submit" class="btn btn-inverse">Rechercher</button>
+                </span>
+              </div>
+            </form>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 
-<!-- <div class="col-sm-1"> -->
-<!-- <ul class="vertical"> -->
-  <!-- <li><a href="#">Home</a></li>
-  <li><a href="#">News</a></li>
-</ul>
-<ul class="vertical">
-  <li><a href="#">Home</a></li>
-  <li><a href="#">News</a></li>
-</ul>
-<ul class="vertical">
-  <li><a href="#">Home</a></li>
-  <li><a href="#">News</a></li>
-</ul> -->
-<!-- </div> -->
-
-<div class="col-sm-11">
-  <div class="row">
-
+  <div class="masonry">
     <?php
     foreach($reponses as $reponse){
-      echo"<div class='cadre col-sm-3'>";
+      echo"<div class='item'>";
       echo "<a href='affichage.php?id=" . $reponse["id"] . "'>";
-      echo"<div class='border'>";
-      echo "<img class='ca'src='" . $reponse["IMAGES"] . "'>";
-      echo "</div>";
-      echo "<div class='texte'>";
-      echo "<h3>" . $reponse["NOM DU MUSEE"] . "</h3>";
+      echo "<figure class='img'>";
+      echo "<img src='" . $reponse["IMAGES"] . "'>";
+      echo "<figcaption class='menu'>";
+      echo "<h3>" . $reponse["NOMDUMUSEE"] . "</h3>";
       echo "<p>" . $reponse["ADR"] . " " . $reponse["VILLE"] . "</p>";
-      echo "</div>";
+      echo "</figcaption>";
+      echo "</figure>";
       echo "</a>";
       echo "</div>";
     }
-     ?>
+
+    ?>
   </div>
-</div>
-
-<footer>
- <div class="col-sm-12">
-   <div class="row">
-     <div class="col-sm-3"></div>
-     <div class="col-sm-3"></div>
-     <div class="col-sm-3"></div>
-     <div class="col-sm-3"></div>     
-
- </div>
- </div>
+<footer class="w3-container w3-padding-64 w3-light-grey w3-center w3-opacity" style="margin-top:128px">
+  <div class="w3-xlarge w3-padding-32">
+    <a href="#" class="w3-hover-text-indigo"><i class="fa fa-facebook-official"></i></a>
+    <a href="#" class="w3-hover-text-red"><i class="fa fa-pinterest-p"></i></a>
+    <a href="#" class="w3-hover-text-light-blue"><i class="fa fa-twitter"></i></a>
+    <a href="#" class="w3-hover-text-grey"><i class="fa fa-flickr"></i></a>
+    <a href="#" class="w3-hover-text-indigo"><i class="fa fa-linkedin"></i></a>
+  </div>
+  <p style="font-weight:normal">Powered by <a href="http://www.w3schools.com/w3css/default.asp" target="_blank" class="w3-hover-text-green">w3.css</a></p>
 </footer>
+<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+
+<script type="text/javascript" language="javascript">
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    if (scroll >= 200) {
+        $(".menu").addClass("fixed");
+    } else {
+        $(".menu").removeClass("fixed");
+    }
+});
+</script>
 </body>
 </html>
